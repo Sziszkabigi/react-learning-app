39d5f77fa1599355aa64ca34c6dc3522dc1b54a0
Brigitta Szikszai <sziszka0001@gmail.com>
2021-04-22 20:08:27 +0100

    add the task

---
+++ src/Table.js
@@ -0,0 +1,77 @@
+import React, { Component } from 'react';
+
+/*class Table extends Component {
+    
+    render() { 
+        return ( 
+            <table>
+                <thead>
+                    <tr>
+                        <th> Name </th>
+                        <th> Job </th>
+                    </tr>
+                </thead>
+                <tbody>
+                <tr>
+                    <td>Charlie</td>
+                    <td>Janitor</td>
+                </tr>
+                <tr>
+                    <td>Mac</td>
+                    <td>Bouncer</td>
+                </tr>
+                <tr>
+                    <td>Dee</td>
+                    <td>Aspiring actress</td>
+                </tr>
+                <tr>
+                    <td>Dennis</td>
+                    <td>Bartender</td>
+                </tr>
+
+                </tbody>
+            </table>
+         );
+    }
+}
+ 
+export default Table;*/
+const TableHeader = () => {
+    return (
+      <thead>
+        <tr>
+          <th>Name</th>
+          <th>Job</th>
+        </tr>
+      </thead>
+    );
+  }
+  /*You'll notice I've added a key index to each table row. You should always use keys when making lists in React, as they help identify each list item. We'll also see how this is necessary in a moment when we want to manipulate list items. */
+
+  const TableBody = (props) => {
+    const rows = props.characterData.map((row, index) => {
+        return (
+            <tr key={index}>
+            <td>{row.name}</td>
+            <td>{row.job}</td>
+            <td>
+                <button onClick={() => props.removeCharacter(index)}>Delete</button>
+            </td>
+            </tr>
+    );
+  });
+
+  return <tbody>{rows}</tbody>
+}
+
+
+const Table = (props) => {
+    const { characterData, removeCharacter } = props
+        return (
+            <table>
+                <TableHeader />
+                <TableBody characterData={characterData} removeCharacter={removeCharacter}>Delete</TableBody> 
+            </table>
+        )
+}
+export default Table;
